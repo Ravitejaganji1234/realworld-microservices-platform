d3945451737e6b20464ee5118a5f201c33547bcd
Ravitejaganji1234 <MTL1017@middleware.in>
2026-02-10 16:26:23 +0530

    changed all services controllers

--- catalogservice/src/main/java/com/example/catalogservice/Controller/CatalogController.java
+++ catalogservice/src/main/java/com/example/catalogservice/Controller/CatalogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/catalog")
+@RequestMapping("/catalog")
 public class CatalogController {
 
     private final ProductRepository repo;
