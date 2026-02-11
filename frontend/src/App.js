0ab3ed7169411727a8ff64bbc1272bd01237f454
Ravitejaganji1234 <MTL1017@middleware.in>
2026-02-11 16:58:32 +0530

    changed the Heading of the application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Catalog from "./components/Catalog";
 export default function App() {
   return (
     <div style={{ padding: "20px" }}>
-      <h2>K8s Real-World Practice App - Ravi Teja Ganji</h2>
+      <h2>K8s Real-World Practice App - Ravi</h2>
 
       <Users />
       <hr />
